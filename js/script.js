125a8e2970745296732a090fd681e46f88abcad2
Sara <saragorleta@yahoo.com>
2021-01-12 15:41:29 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $('.with-dropdown').hover(function(){
+    // $('.dropdown-menu').toggleClass('active');
+
+    //ipotesi più dropdown
+    $(this).children('.dropdown-menu').toggleClass('active');
+  });
+
+
+});
