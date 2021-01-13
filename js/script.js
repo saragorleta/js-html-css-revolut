dcf6202cc4f97c92c813bc8da3728f05c47bdb21
Sara <saragorleta@yahoo.com>
2021-01-13 15:56:45 +0100

    add evidenzia dropdown-menu

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
+  // funzione che abilita sia dropdown-menu sia evidenzia
+  $('.show').hover(function(){   //quando passo il mouse sulla classe show..
 
-  $('.show').hover(function(){
-    // $('.dropdown-menu').toggleClass('active');
-
-    //ipotesi più dropdown
+    //ipotesi quando troviamo più dropdown -->this
+    //su questa(this) classe figlia (children.dropdown-menu) tolgo e metto(toggleClasse)la classe .active
     $(this).children('.dropdown-menu').toggleClass('active');
+    //su questa(this) classe figlia (children.evidenzia) tolgo e metto(toggleClasse)la classe .active
     $(this).children('.evidenzia').toggleClass('active');
   });
+  // funzione che permette di effettuare il click
   $('.click').click(function(){
     $(this).children('.dropdown-menu').toggleClass('active');
     $(this).children('.evidenzia').toggleClass('active');
 
   });
+  $('.dropdown-menu').hover(function(){
+  $(this).children('.evidenzia').toggleClass('active');
+});
 });
